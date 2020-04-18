395148a91767091ede55f9ab4a74eda6ce1a354c
Holden Hinkle <holdenhinkle@gmail.com>
2020-04-18 08:21:43 -0400

    add storage to sdk object

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,19 @@
 import authFactory from './auth';
 import dbFactory from './db';
 import createNewWebsocket from './ws';
+import storageFactory from './storage';
 
 const createSdk = (url) => {
   const auth = authFactory(url);
   const db = dbFactory(url);
   const ws = () => createNewWebsocket(url);
+  const storage = storageFactory(url);
 
   return {
     auth,
     db,
     ws,
+    storage,
   }
 };
 
