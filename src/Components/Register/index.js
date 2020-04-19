b30acb1db34dba1e52576e2b920b98e78ad27e7b
Holden Hinkle <holdenhinkle@gmail.com>
2020-04-18 20:07:00 -0400

    have user provide a username

--- src/Components/Register/index.js
+++ src/Components/Register/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Register extends React.Component {
   state = {
+    username: '',
     email: '',
     password: '',
   }
@@ -16,7 +17,9 @@ class Register extends React.Component {
 
   handleRegisterSubmit = (e) => {
     e.preventDefault();
+
     this.props.handleRegisterSubmit({
+      username: this.state.username,
       email: this.state.email,
       password: this.state.password,
     });
@@ -27,6 +30,10 @@ class Register extends React.Component {
       <div>
         <h1>Register</h1>
         <form onSubmit={this.handleRegisterSubmit}>
+          <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" value={this.state.username} required onChange={this.handleOnChange} />
+          </div>
           <div>
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" value={this.state.email} required onChange={this.handleOnChange} />
